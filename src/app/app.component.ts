860f2d2a94ec302c3f083c94a3c23d75618be280
masixolematolweni.1@gmail.com <72172920+Matolweni95@users.noreply.github.com>
2022-01-27 13:17:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'darkMode';
+
+  func(){
+    const target = document.getElementsByClassName('container')[0].classList.toggle('dark-mode');
+    const ver = document.getElementsByClassName('card')[0].classList.toggle('dark-mode');
+  }
 }
